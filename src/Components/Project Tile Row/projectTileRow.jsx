ce4e1cabb95e3722595b365c5fa65a236091cb6b
ryguysir <ryanrigley@gmail.com>
2024-06-03 00:50:58 +0000

    added in the arrow

--- src/Components/Project Tile Row/projectTileRow.jsx
+++ src/Components/Project Tile Row/projectTileRow.jsx
@@ -7,29 +7,35 @@ const ProjectTileRow = memo(function ProjectTileRow({ header, children }) {
 
 	const ref = useRef(null);
 	const cover = useRef(null)
-	useEffect(()=>{
-		const scrollListener = (e)=>{
-			if(e.target.scrollLeft>=1){
+	const arrow = useRef(null);
+	useEffect(() => {
+		const scrollListener = (e) => {
+			if (e.target.scrollLeft >= 1) {
 				cover.current.style.setProperty('--coverOpacity', '0');
 				cover.current.style.setProperty('--coverLeftAdjust', `-${e.target.scrollLeft}px`);
-			} else{
+				arrow.current.style.setProperty('display', `none`);
+
+			} else {
 				cover.current.style.setProperty('--coverOpacity', '1');
 				cover.current.style.setProperty('--coverLeftAdjust', '0px');
+				arrow.current.style.setProperty('display', `block`);
 			}
 		}
 		ref.current.addEventListener("scroll", scrollListener);
-	},[ref])
-	
-  return (
-    <div>
-      <div className="project-tile-row-header">{header}</div>
-			
-      	<div className="project-tile-row" ref={ref}>
-					<div className="project-tile-row-cover" ref={cover}>{children}</div>
-				</div>
-			
-    </div>
-  )
+	}, [ref])
+
+	return (
+		<div>
+			<div className="project-tile-row-header">{header}</div>
+
+			<div className="project-tile-row" ref={ref}>
+				<i ref={arrow} className="fa-solid fa-arrow-right project-tile-arrow" />
+				<div className="project-tile-row-cover" ref={cover}>{children}</div>
+
+			</div>
+
+		</div>
+	)
 });
 
 export default ProjectTileRow;
